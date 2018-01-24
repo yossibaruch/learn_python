9c6c47648f2b60586f724634d610d4cc4cfc592a
yossib <yossi.baruch@iucc.ac.il>
2018-01-25 00:32:51 +0200

    quicksort continues

--- just for fun/fun_quicksort.py
+++ just for fun/fun_quicksort.py
@@ -2,28 +2,30 @@ def quicksort(A, lowerq, upperq):
     print(A, lowerq, upperq, "in quicksort")
     if lowerq < upperq:
         p = partition(A, lowerq, upperq)
-        quicksort(A, lowerq, p-1)
-        quicksort(A, p+1, upperq)
+        quicksort(A, lowerq, p - 1)
+        quicksort(A, p + 1, upperq)
     return A
 
 
 def partition(A, lowerp, upperp):
     print(A, lowerp, upperp, "in partition")
     pivot = A[lowerp]
-    left = lowerp+1
-    right = upperp
-    for j in range(left, right):
-        if A[j] > pivot:
-            left = left+1
-            A[left], A[j] = A[j], A[left]
-    if A[upperp] < A[i+1]:
-        A[i+1], A[upperp] = A[upperp], A[i+1]
-    return i+1
+    index_l = lowerp + 1
+    index_r = upperp
+
+    while index_l < index_r:
+        while (A[index_l] < pivot) and (index_l <= index_r): index_l += 1
+        while (A[index_r] > pivot) and (index_l <= index_r): index_r -= 1
+        A[index_l], A[index_r] = A[index_r], A[index_l]
+        print(A, index_l, index_r, "in partition in while")
+    A[lowerp], A[index_l] = A[index_l], A[lowerp]
+    print(A, index_l, index_r, "in partition end")
+    return index_r
 
 
-A = [1, 2, 9, 3, 7, 4, 10, 8, 6, 5]
+A = [5, 2, 3, 10, 1, 8, 6, 4, 7]
 
-print(A, 0, len(A)-1)
+print(A, 0, len(A) - 1)
 # print(A[lower], A[upper])
 
-print(quicksort(A, 0, len(A)-1))
+print(quicksort(A, 0, len(A) - 1))
