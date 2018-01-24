8c9acf33eaf4113753794dd368d875d14a1a8491
yossib <yossi.baruch@iucc.ac.il>
2018-01-24 22:41:28 +0200

    mission01 part 2

--- just for fun/fun_quicksort.py
+++ just for fun/fun_quicksort.py
@@ -1,5 +1,5 @@
 def quicksort(A, lowerq, upperq):
-    # print(A, lowerq, upperq)
+    print(A, lowerq, upperq, "in quicksort")
     if lowerq < upperq:
         p = partition(A, lowerq, upperq)
         quicksort(A, lowerq, p-1)
@@ -8,27 +8,24 @@ def quicksort(A, lowerq, upperq):
 
 
 def partition(A, lowerp, upperp):
-    # print(A, lowerp, upperp)
-    pivot = A[upperp]
-    i = lowerp
-    range
-    print(q)
-    for j in range(lowerp, upperp-1):
-        if A[j] < pivot:
-            i = i+1
-            A[i], A[j] = A[j], A[i]
+    print(A, lowerp, upperp, "in partition")
+    pivot = A[lowerp]
+    left = lowerp+1
+    right = upperp
+    for j in range(left, right):
+        if A[j] > pivot:
+            left = left+1
+            A[left], A[j] = A[j], A[left]
     if A[upperp] < A[i+1]:
         A[i+1], A[upperp] = A[upperp], A[i+1]
     return i+1
 
 
-A = [1, 2, 9, 3, 7, 2, 3, 8, 2, 5]
-lower = 0
-upper = len(A)-1
+A = [1, 2, 9, 3, 7, 4, 10, 8, 6, 5]
 
-print(A, lower, upper)
+print(A, 0, len(A)-1)
 # print(A[lower], A[upper])
 
-print(quicksort(A, lower, upper))
+print(quicksort(A, 0, len(A)-1))
 
 
