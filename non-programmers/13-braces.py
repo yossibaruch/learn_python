02f8b67da4cc1666bc2decced63e914de80fea5a
yossib <yossi.baruch@iucc.ac.il>
2019-01-16 20:36:08 +0200

    ex 13 braces done

--- non-programmers/13-braces.py
+++ non-programmers/13-braces.py
@@ -9,6 +9,27 @@ print("""
 # If memory of opening braces is not empty - return False
 # else return True
 """)
+
+
+def hebrew_braces(braces_list):
+    braces_work = []
+    for c in braces_list:
+        if c in '{[(':
+            braces_work.append(c)
+        if c in '}])':
+            try:
+                b = braces_work.pop()
+            except IndexError:
+                return False
+            if (b == '(' and c == ')') or (b == '[' and c == ']') or (b == '{' and c == '}'):
+                continue
+            else:
+                print("Not balanced braces")
+                return False
+    print("Balanced braces")
+    return True
+
+
 while True:
     try:
         bracesWithChars = str(input("Please enter a string with braces:"))
@@ -30,22 +51,4 @@ for let in list(whatToRemove):
 bracesOnly = list(bracesOnly)
 print(bracesOnly)
 
-if len(bracesOnly) % 2 == 1:
-    print("not even number of braces")
-    exit(False)
-
-braces = []
-
-for c in bracesOnly:
-    if c in '{[(':
-        braces.append(c)
-    if c in '}])':
-        b = braces.pop()
-        if (b == '(' and c == ')') or (b == '[' and c == ']') or (b == '{' and c == '}'):
-            continue
-        else:
-            print("Not balanced braces")
-            exit(False)
-
-print("Balanced braces")
-exit(True)
+hebrew_braces(bracesOnly)
