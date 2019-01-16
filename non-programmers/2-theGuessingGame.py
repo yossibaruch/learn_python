165797a2a3dae53c73c694447d3793929e15fc83
yossib <yossi.baruch@iucc.ac.il>
2019-01-16 15:12:21 +0200

    added pfnp

--- non-programmers/2-theGuessingGame.py
+++ non-programmers/2-theGuessingGame.py
@@ -10,7 +10,11 @@ print("""
 number = random.randint(1, 100)
 
 while 1 == 1:
-    guessed = int(input("number: "))
+    try:
+        guessed = int(input("number: "))
+    except ValueError:
+        print("That was not an int!!!")
+        continue
     print("Correct!!!" if number == guessed else "wrong! Guess again... HINT: ")
     exit(0) if number == guessed else print(">" if number > guessed else "<")
 
