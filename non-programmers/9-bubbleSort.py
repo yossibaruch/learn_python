1399f084a922da36228fd606fec3d855259db37e
yossib <yossi.baruch@iucc.ac.il>
2019-01-16 17:27:32 +0200

    ex 9 bubble sort done

---
+++ non-programmers/9-bubbleSort.py
@@ -0,0 +1,26 @@
+print("""
+# Get list of items
+# Bubble sort the list:
+#    - Iterate on all indices
+#    - if adjacent items are out of order - swap them
+#    - repeat n times (n is list length)
+""")
+array = []
+while True:
+    try:
+        array = [int(x) for x in input().split()]
+    except ValueError:
+        print("This is not a list of integers, please make corrections")
+        continue
+    break
+
+print(array)
+print(sorted(array))
+
+while not array == sorted(array):
+    for i in range(len(array)-1):
+        print(array[i], array[i+1])
+        array[i], array[i+1] = (array[i], array[i+1]) if array[i] < array[i+1] else (array[i+1], array[i])
+        print(array[i], array[i + 1])
+
+print(array)
