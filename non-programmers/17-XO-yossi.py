eb8c7477a78ad9e1befabf8769e711dc45c5757b
yossib <yossi.baruch@iucc.ac.il>
2019-02-27 20:43:42 +0200

    what's left os to build Game Class

--- non-programmers/17-XO-yossi.py
+++ non-programmers/17-XO-yossi.py
@@ -20,7 +20,44 @@ class Board:
     def to_string(self):
         print("{}|{}|{}\n-----\n{}|{}|{}\n-----\n{}|{}|{}\n".format(*self.board))
 
+    def make_move(self, player, place):
+        self.board[place] = player.marker
+        return self.is_winner(player.marker)
+
+    def is_winner(self, marker):
+        print(marker)
+        winning_sets = [
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6}
+        ]
+        state = [i for i, val in enumerate(self.board) if val == marker]
+        self.to_string()
+        for i in winning_sets:
+            if i.issubset(state):
+                print(f'Player with {marker} has won')
+                print(state)
+                return True
+        return False
+
 
 yossi = Player("yossi", "X")
+shoshy = Player("shoshy", "Y")
 board = Board()
 print(board.to_string())
+
+board.make_move(yossi, 0)
+board.make_move(shoshy, 1)
+board.make_move(yossi, 3)
+board.make_move(shoshy, 4)
+board.make_move(yossi, 5)
+board.make_move(shoshy, 2)
+board.make_move(yossi, 6)
+board.make_move(shoshy, 7)
+board.make_move(yossi, 8)
+
