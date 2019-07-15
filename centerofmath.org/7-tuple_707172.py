d88b519da01b2d5551b615d687b5028246828281
yossib <yossi.baruch@iucc.ac.il>
2019-07-15 19:01:27 +0300

    center of math - 7-tuple

---
+++ centerofmath.org/7-tuple_707172.py
@@ -0,0 +1,7 @@
+"""
+Find the number of 7-tuple of positive integers (a,b,c,d,e,f,g) that satisfy:
+    a*b*c = 70
+    c*d*e = 71
+    e*f*g = 72
+"""
+print("please find the 7-tuple (a,b,c,d,e,f,g) that fits the criteria a*b*c = 70, c*d*e = 71, e*f*g = 72")
